d756669b56bc8b13ce90c6d6ea240d50c0c970f9
sproshchaev <sproshchaev@gmail.com>
2024-05-17 18:26:48 +0500

    upd.

--- m1l2-basic/src/main/kotlin/com/prosoft/Main2.kt
+++ m1l2-basic/src/main/kotlin/com/prosoft/Main2.kt
@@ -4,6 +4,6 @@ package com.prosoft
  * Второй вариант точки входа в Kotlin (с аргументами командной строки)
  */
 fun main(vararg args: String) {
-    println("Hello ${args}")                     // Hello [Ljava.lang.String;@15aeb7ab
+    println("Hello $args")                       // Hello [Ljava.lang.String;@15aeb7ab
     println("Hello ${args.contentToString()}")   // Hello []
 }
--- m1l2-basic/src/main/kotlin/com/prosoft/Main3.kt
+++ m1l2-basic/src/main/kotlin/com/prosoft/Main3.kt
@@ -4,6 +4,6 @@ package com.prosoft
  * Третий вариант точки входа в Kotlin (с аргументами командной строки)
  */
 fun main(args: Array<String>) {
-    println("Hello ${args}")                     // Hello [Ljava.lang.String;@15aeb7ab
+    println("Hello $args")                       // Hello [Ljava.lang.String;@15aeb7ab
     println("Hello ${args.contentToString()}")   // Hello []
 }
