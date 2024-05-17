f586ddf74a0973d4b611ad060ab3a62a0b116a32
sproshchaev <sproshchaev@gmail.com>
2024-05-17 14:56:20 +0500

    upd.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,21 @@
+/** В корневом settings.gradle.kts необходимо соблюдать следующую очередность (как здесь) */
+
+/** 1) имя проекта */
+rootProject.name = "kotlin-demo"
+
+/** 2) раздел pluginManagement, где settings - ссылка на файл gradle.properties в котором прописана kotlinVersion */
+pluginManagement {
+    val kotlinVersion: String by settings
+
+    plugins {
+        kotlin("jvm") version kotlinVersion
+    }
+}
+
+/** 3) раздел plugins */
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
 }
-rootProject.name = "kotlin-demo"
 
+/** 4) include */
+include("m1l1-first")
